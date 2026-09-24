2a37923fe12c65f85705721eef9bc70ca5ea0c40
dole360 <muaisds@gmail.com>
2026-09-24 13:13:08 +0700

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbwjxamEm78z1EzEg29ZdAOlsicha9sBB_c0wqYaVU8vqm8YBcbk1fxlOeApojTfJkb9/exec'
+  });
+})();
